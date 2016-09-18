797c12efb2ccda5e070ac94773f219f66af870f3
ryabkov79 <ryabkov79@gmail.com>
2016-09-18 15:22:53 +0300

    Улучшена валидация, добавлен календарь.

--- form/assets/js/form.js
+++ form/assets/js/form.js
@@ -1,13 +1,34 @@
 (function ($) {
   $(document).ready(function () {
+    $("#datepicker_icon").click(function(){
+      $("#bdate").datepicker( "show" );
+    });
+
+    $("#bdate").datepicker({
+      changeMonth: true,
+      changeYear: true,
+      maxDate: "+0d",
+      yearRange: "c-70:c+0"
+    });
+
     $("#reg-card").validate({
       rules: {
-        fname: "required",
-        sname: "required",
-        pname: "required",
+        fname: {
+          required: true,
+          lettersAndSpace: true,
+        },
+        sname: {
+          required: true,
+          lettersAndSpace: true,
+        },
+        pname: {
+          required: true,
+          lettersAndSpace: true,
+        },
         phone: {
 					required: true,
 					minlength: 10,
+					maxlength: 10,
           digits: true,
 				},
         email: {
@@ -17,10 +38,14 @@
         card_num: {
 					required: true,
           digits: true,
+          minlength: 12,
+          maxlength: 12,
 				},
         card_pin: {
 					required: true,
           digits: true,
+          minlength: 4,
+          maxlength: 4,
 				},
         sms_code: {
 					required: true,
@@ -30,21 +55,35 @@
         rules_agreement: "required",
       },
       messages: {
-        fname: "Введите Вашу фамилию",
-        sname: "Введите Ваше имя",
-        pname: "Введите Ваше отчество",
+        fname: {
+          required: "Введите Вашу фамилию",
+          lettersAndSpace: "Только буквы и пробел",
+        },
+        sname: {
+          required: "Введите Ваше имя",
+          lettersAndSpace: "Только буквы и пробел",
+        },
+        pname: {
+          required: "Введите Ваше отчество",
+          lettersAndSpace: "Только буквы и пробел",
+        },
         phone: {
           required: "Введите номера телефона",
           digits: "Только цифры",
           minlength: "10 цифр",
+          maxlength: "10 цифр",
         },
         card_num: {
           required: "Введите номер карты",
           digits: "Только цифры",
+          minlength: "12 цифр",
+          maxlength: "12 цифр",
         },
         card_pin: {
           required: "Введите ПИН-код карты",
           digits: "Только цифры",
+          minlength: "4 цифры",
+          maxlength: "4 цифры",
         },
         sms_code: {
           required: "Введите код активации из СМС",
@@ -62,4 +101,8 @@
 			},
     });
   });
+
+  jQuery.validator.methods.lettersAndSpace = function( value, element ) {
+    return this.optional( element ) || /^[а-яa-z\s]+$/.test( value );
+  }
 })(jQuery);
